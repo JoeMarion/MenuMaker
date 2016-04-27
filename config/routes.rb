c8f8928a671063816b4ca01a44a3b531b1a7e9df
= <joeyzmarion@gmail.com>
2016-04-27 02:20:26 -0500

    Add sidemenu with jquery & log in/log out links

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+
+  root to: "pages#home"
   devise_for :users
 
   devise_scope :user do
     get 'login', to: "devise/sessions#new"
     get 'signup', to: "devise/registrations#new"
+    get 'logout', to: "devise/sessions#destroy"
   end
 end
